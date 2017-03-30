1495f79202a5fac1e2551f44802a03ea3bfd38a5
Gustavo Isidoro <tartuzet@gmail.com>
2017-03-30 16:10:10 -0600

    clase 1

---
+++ arrow_function.js
@@ -0,0 +1,8 @@
+let base = 5
+let altura = 9
+const areaTriangulo = (base,altura) => base*altura/2
+console.log(`El área de un triangulo de base ${base} y altura ${altura} es: ${areaTriangulo(base,altura)}`)
+
+let radio = 7
+const areaCirculo = radio => Math.PI * (radio * radio)
+console.log(`El área de un círculo con radio ${radio} es: ${areaCirculo(radio)}`)
